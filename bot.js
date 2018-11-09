4165fd6cd07e0084b3a0b13e9dea856b525a0484
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2018-11-09 11:54:42 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
-const {run, get} = require('node-cmd');
-console.log("Hello World");
+const Discord = require("discord.js");
+const bot = new Discord.Client({autorun: true});
 
-get("cd tbg & node index.js", () => {
-  console.log("Hello World");
-  get("go north", (err, stdout, stderr) => console.log(stdout));
+bot.on("ready", () => {
+  console.log("I am ready!");
+});
+
+bot.on("message", msg => {
+  
+  if (msg.content.substring(0, 1) == "!") {
+    const cmd = msg.content.substring(1);
+    msg.channel.send(cmd);
+  }
+   
+
 });
