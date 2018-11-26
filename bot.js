5440d6c336e5de7d07f3a4cbccd52320cfdb21c8
Ethan <ethan.r.davidson@gmail.com>
2018-11-25 21:42:32 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,17 +13,17 @@ bot.on("message", msg => {
 
   if (msg.content.substring(0, 1) == "!") {
     const cmd = msg.content.substring(1);
-    if (cmd == "START_MAZE") {
+    if (cmd == "start maze") {
       Game = require('./tbg/game.js');
       Game.setChannel(msg.channel);
       Game.play('./tbg/game_data.json');
-    } else if (cmd == "END_MAZE") {
+    } else if (cmd == "end maze") {
       Game = undefined;
-      msg.channel.send("Ended the game.");
+      msg.channel.send("Ended the game. Thanks for playing <3");
     } else if (Game) {
       Game.setChannel(msg.channel);
       Game.nextMsgFn(cmd);
     }
   }
-  
+
 });
